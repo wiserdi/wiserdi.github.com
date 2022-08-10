5e9c58544bb3f178e25c5edb6d7c1ca52d2eb8c7
WiseDi <wisenana@outlook.com>
2022-08-11 02:33:35 +0800

    Site updated: 2022-08-11 02:33:34

--- stuff/Name_changer(period).py
+++ stuff/Name_changer(period).py
@@ -7,7 +7,7 @@ def test(data_path, length):
     print(os.listdir(data_path))
     for num in range(0, length):
         file_name = os.listdir(data_path)[num]
-        new_name = file_name.replace('Hong Kong', 'HandK')  # 替换
+        new_name = file_name.replace(' ', '_')  # 替换
         os.rename(data_path + file_name, data_path + new_name)
 
 test('C:/Users/AndyC/wiserdi.github.io/source/stuff/', len(os.listdir("C:/Users/AndyC/wiserdi.github.io/source/stuff/")))
